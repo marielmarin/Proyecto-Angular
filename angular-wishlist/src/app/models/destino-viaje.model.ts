8843a866fb7c1e7c0ef618a196dfea14947b3d08
Mariel <marinmariel94@gmail.com>
2020-12-06 21:08:38 -0600

    subida Proyecto wish-list

---
+++ angular-wishlist/src/app/models/destino-viaje.model.ts
@@ -0,0 +1,14 @@
+export class DestinoViaje {
+    
+    
+    nombre:string;
+    imagenUrl:string;
+
+    constructor(n:string, u:string){
+
+        this.nombre = n ;
+        this.imagenUrl = u;
+
+
+    }
+}
